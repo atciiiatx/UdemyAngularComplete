988b8a704222511e6a97d9ee35cbfc47e330ca32
atciii <atcampbelliii@gmail.com>
2022-12-13 17:55:23 -0600

    Update Section001 to Angular 15

--- Section001/my-first-app/src/app/app.module.ts
+++ Section001/my-first-app/src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
